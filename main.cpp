13cb95ff4cb83edb50795a0ba7026d724038bad5
Alexnott <34028130+Alexnott@users.noreply.github.com>
2018-03-02 09:07:27 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include "stdafx.h"
+
+#include <iostream>
+
+#include <complex>
+
+
+
+using namespace std;
+
+
+
+struct complex_t 
+
+{
+
+	float real;
+
+	float imag;
+
+};
+
+std::istream & read(std::istream & stream, complex_t & complex) 
+
+{
+
+	char c;
+
+	float real;
+
+	float imag;
+
+	if (stream >> c && c == '(' && 
+
+		stream >> real && 
+
+		stream >> c && c == ',' &&
+
+		stream >> imag &&
+
+		stream >> c && c == ')') 
+
+	{
+
+		complex.real = real;
+
+		complex.imag = imag;
+
+	}
+
+
+
+	else 
+
+	{
+
+		std::cout << "An error has occured while reading input data";
+
+	}
+
+	return stream;
+
+
+
+}
+
+std::ostream & write(std::ostream & stream, complex_t & complex)
+
+{
+
+	if (stream << "(" && stream << complex.real &&
+
+	    stream << "," && 
+
+	    stream << complex.imag && stream << ")")
+
+		;
+
+	return stream;
+
+}
+
+int main()
+
+{
+
+	complex_t compl1;
+
+	complex_t compl2;
+
+	char zn;
+
+	if(read(cin,compl1)&&(cin>>zn)&&read(cin,compl2))
+
+	{
+
+		switch (zn)
+
+		{
+
+			case '+':
+
+				compl1.real = compl1.real + compl2.real;
+
+				compl1.imag = compl1.imag + compl2.imag;
+
+				break;
+
+			case '-':
+
+				compl1.real = compl1.real - compl2.real;
+
+				compl1.imag = compl1.imag - compl2.imag;
+
+				break;
+
+			case '*':
+
+				compl1.real = compl1.real*compl2.real - compl1.imag*compl2.imag;
+
+				compl1.imag = compl1.real*compl2.imag + compl1.imag*compl2.real;
+
+				break;
+
+			case '/`':
+
+				compl1.real = (compl1.real*compl2.real - compl1.imag*compl2.imag*(-1)) / (compl2.real*compl2.real - compl2.imag*compl2.imag);
+
+				compl1.real = (compl1.imag*compl2.real - compl1.real*compl2.imag*(-1)) / (compl2.real*compl2.real - compl2.imag*compl2.imag);
+
+				break;
+
+			default:
+
+				std::cout << "An error has occured while reading input data";
+
+				break;
+
+		}
+
+	}
+
+	
+
+	write(cout,compl1);
+
+}
